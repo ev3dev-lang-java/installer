7b7d2244062c16471086438e4ecaeee5c65734b3
Juan Antonio Breña Moral <bren@juanantonio.info>
2017-05-13 19:35:38 +0100

    Minor change

--- installer.sh
+++ installer.sh
@@ -59,7 +59,7 @@ if [ "$PLATFORM" == "$EV3" ];
 then
     echo $EV3
     wget http://ftp.us.debian.org/debian/pool/main/b/bluez/libbluetooth-dev_5.23-2+b1_armel.deb
-    dpkg -i libbluetooth-dev_5.23-2+b1_armel.deb
+    sudo dpkg -i libbluetooth-dev_5.23-2+b1_armel.deb
 else
     echo $BRICKPI
     isInstalled libbluetooth-dev
