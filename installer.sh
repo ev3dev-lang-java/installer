b34446b0f3987a256228fae3bb1293a26398d898
Juan Antonio Breña Moral <bren@juanantonio.info>
2017-05-13 18:52:52 +0100

    Installing bluetooth

--- installer.sh
+++ installer.sh
@@ -54,6 +54,8 @@ echo ""
 if [PLATFORM=="EV3" ]
 then
     echo "EV3"
+    wget http://ftp.us.debian.org/debian/pool/main/b/bluez/libbluetooth-dev_5.23-2+b1_armel.deb
+    dpkg -i libbluetooth-dev_5.23-2+b1_armel.deb
 else
     echo "BRICKPI"
     apt get install libbluetooth-dev
