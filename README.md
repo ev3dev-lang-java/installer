89835aa2d06959bb9a9315580900ffa2540fc7fc
Juan Antonio Breña Moral <bren@juanantonio.info>
2017-05-13 18:32:34 +0100

    Minor change

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # installer
 A Bash script to install everything related with EV3Dev-lang-java
 
+## Getting Started
+
+In order to use the project, is is better if you install some libraries.
+
+```
+wget -c https://raw.githubusercontent.com/ev3dev-lang-java/installer/develop/installer.sh
+chmod +x installer.sh
+./installer.sh
+```
+
 
--- installer.sh
+++ installer.sh
@@ -14,8 +14,8 @@ else
 fi
 
 #1. Detect Java
-#1.1 Install Java
-#1.2 Create JAVA_HOME
+#1.1 Install Java PENDING
+#1.2 Create JAVA_HOME PENDING
 if type -p java; then
     echo found java executable in PATH
     _java=java
@@ -24,16 +24,6 @@ elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
     _java="$JAVA_HOME/bin/java"
 else
     echo "no java"
-fi
-
-if [[ "$_java" ]]; then
-    version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
-    echo version "$version"
-    if [[ "$version" > "1.8" ]]; then
-        echo version is more than 1.5
-    else
-        echo version is less than 1.5
-    fi
 fi
 
 #2. Install Bluetooth
