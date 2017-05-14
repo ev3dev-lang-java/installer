f3ad1f3a1eaf302f61cac8150be0aff9aee36131
Juan Antonio Breña Moral <bren@juanantonio.info>
2017-05-14 16:28:23 +0100

    Minor change

--- modules/utilities.sh
+++ modules/utilities.sh
@@ -36,7 +36,7 @@ function createHeader() {
 INSTALLED="NO";
 function isInstalled(){
     echo "Parameter #1 is $1"
-    local result = dpkg-query -W -f='${Status}\n' $1 | head -n1 | awk '{print $3;}' | grep -q '^installed$'
+    local result=$(dpkg-query -W -f='${Status}\n' $1 | head -n1 | awk '{print $3;}' | grep -q '^installed$')
     echo result
     INSTALLED=$result;
 }
