eedf05dcb25f17cf3c66bf70d24f73831706d550
Juan Antonio Breña Moral <bren@juanantonio.info>
2017-05-14 18:32:18 +0100

    Minor change

--- modules/help.sh
+++ modules/help.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+echo "Installer options:"
 echo "sudo ./installer.sh [Execute modules: platform, java & batteryMonitor]"
 echo "sudo ./installer.sh extended [Execute modules: platform, java, batteryMonitor & native-libraries]"
 echo "sudo ./installer.sh jdk robot@192.168.1.165 [Copy your JRE to a remote brick]"
+echo
 exit
