31a71376720b97213f999e573aac8108e5027e7b
Juan Antonio Breña Moral <bren@juanantonio.info>
2017-05-14 18:00:27 +0100

    Minor change

--- modules/java.sh
+++ modules/java.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-function installJava() {
+function installJavaForBrickPi() {
     apt-key adv --recv-key --keyserver keyserver.ubuntu.com EEA14886
     echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | sudo tee -a /etc/apt/sources.list
     echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | sudo tee -a /etc/apt/sources.list
@@ -11,6 +11,20 @@ function installJava() {
     #https://askubuntu.com/questions/190582/installing-java-automatically-with-silent-option
 }
 
+function installJavaForEV3(){
+    if [ -e "/home/robot/ejdk-8-fcs-b132-linux-arm-sflt-03_mar_2014.tar.gz" ]; then
+        tar -zxvf "/home/robot/ejdk-8-fcs-b132-linux-arm-sflt-03_mar_2014.tar.gz" -C /opt
+        update-alternatives --install /usr/bin/java java /opt/ejdk1.8.0/linux_arm_sflt/jre/bin/java 1
+        java -version
+    else
+        echo "Sorry, the installer didn´t detect ejdk-8-fcs-b132-linux-arm-sflt-03_mar_2014.tar.gz"
+        echo "on /home/robot"
+        echo "try to copy the file again to the EV3 Brick."
+        echo
+        exit 1
+    fi
+}
+
 #1. Detect Java
 #1.1 Install Java
 #1.2 Create JAVA_HOME PENDING
@@ -18,36 +32,15 @@ if type -p java; then
     echo "Found java executable in PATH"
     java -version
 elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
-    echo "found java executable in JAVA_HOME"
+    echo "Found java executable in JAVA_HOME"
 else
-    echo "no java"
+    echo "No java detected"
 
     if [ "$PLATFORM" == "$EV3" ]; then
-        echo "You have to follow this documentation to install to continue using this installer"
-        echo "https://github.com/ev3dev-lang-java/docs/blob/master/docs/GettingStartedBrick.md"
-
-if [ -e "/home/robot/ejdk-8-fcs-b132-linux-arm-sflt-03_mar_2014.tar.gz" ]; then
-    echo "ok"
-else
-    echo "nok"
-fi
-
-        #Manual installer of Java 8
-        #wget https://raw.githubusercontent.com/ev3dev-lang-java/installer/develop/modules/java-ev3-java8-installer.sh
-        #source modules/java-ev3-java8-installer.sh
-
+        installJavaForEV3
     elif [ "$PLATFORM" == "$BRICKPI" ]; then
-        installJava
+        installJavaForBrickPi
     fi
 
 fi
 
-if [ -e "/home/robot/ejdk-8-fcs-b132-linux-arm-sflt-03_mar_2014.tar.gz" ]; then
-    echo "ok"
-    tar -zxvf "/home/robot/ejdk-8-fcs-b132-linux-arm-sflt-03_mar_2014.tar.gz" -C /home/robot/
-    #/opt
-    #update-alternatives --install /usr/bin/java java /opt/ejdk1.8.0/linux_arm_sflt/jre/bin/java 1
-    #java -version
-else
-    echo "nok"
-fi
