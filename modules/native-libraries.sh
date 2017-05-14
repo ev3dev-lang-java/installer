9b220a1d606ef02bd39fecd644920a029afa4f73
Juan Antonio Breña Moral <bren@juanantonio.info>
2017-05-14 16:53:19 +0100

    Minor change

--- modules/native-libraries.sh
+++ modules/native-libraries.sh
@@ -6,13 +6,13 @@ if [ "$PLATFORM" == "$UNKNOWN" ]; then
     echo "This platform: $PLATFORM is not suitable for Battery Monitor."
     echo
 else
-    apt-get update
+    #apt-get update
     isInstalled libopencv2.4-java
-    if [ "$INSTALLED" == "$INSTALLED_YES" ]; then
+    if [ "$INSTALLED" == "$INSTALLED_NO" ]; then
         apt-get install libopencv2.4-java
     fi
     isInstalled librxtx-java
-    if [ "$INSTALLED" == "$INSTALLED_YES" ]; then
+    if [ "$INSTALLED" == "$INSTALLED_NO" ]; then
         apt-get install librxtx-java
     fi
 fi
